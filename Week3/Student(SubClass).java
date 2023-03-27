44475705406ee56dc8a1b9a8b5301773a093f85b
xela <128110314+jaclaricia@users.noreply.github.com>
2023-03-27 14:32:21 +0800

    Update Student(SubClass).java

--- Week3/Student(SubClass).java
+++ Week3/Student(SubClass).java
@@ -6,6 +6,7 @@ public class Student {
 	private String schoolYear;
 	private String term;
 	
+	//Constructor
 	public Student(String studentName, String studentNo, String schoolYear, String Term) {
 		this.studentName = studentName;
 		this.studentNo = studentNo;
